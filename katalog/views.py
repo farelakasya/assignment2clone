a99de7e9ac1d7d75cabccdd7ed9c42b568471cdc
Farel Akasya <farelakasya@192.168.1.44>
2022-09-14 00:10:20 +0700

    attempt 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,18 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    list_of_katalogs = CatalogItem.objects.all()
+    context = {
+        'name' : 'Farel Rishad Akasya',
+        'student_Id' : '2106631646',
+        'list_katalog' : list_of_katalogs
+    }
+    return render(
+        request,
+        'katalog.html',
+        context
+    )
